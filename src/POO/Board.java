4c8bcf49e3ba3178aff6a64e8eb4b242e1b27a10
gerard <geriloza@gmail.com>
2020-07-02 17:26:00 +0200

    switch changed

--- src/POO/Board.java
+++ src/POO/Board.java
@@ -17,36 +17,56 @@ public class Board {
 
         jugador.playerPositions.add(pos);
 
-        switch(pos){
-            case 1:
-                taulell[0][0]=jugador.simbol;
-                break;
-            case 2:
-                taulell[0][2]=jugador.simbol;
-                break;
-            case 3:
-                taulell[0][4]=jugador.simbol;
-                break;
-            case 4:
-                taulell[2][0]=jugador.simbol;
-                break;
-            case 5:
-                taulell[2][2]=jugador.simbol;
-                break;
-            case 6:
-                taulell[2][4]=jugador.simbol;
-                break;
-            case 7:
-                taulell[4][0]=jugador.simbol;
-                break;
-            case 8:
-                taulell[4][2]=jugador.simbol;
-                break;
-            case 9:
-                taulell[4][4]=jugador.simbol;
-                break;
-            default:
-                break;
+        int i=1;
+        int j=0, z=0;
+        boolean trobat=false;
+
+        while (i<9 && !trobat){
+            if(pos==i) {
+                trobat=true;
+                taulell[j][z] = jugador.simbol;
+            }
+            if(z<4){
+                z=z+2;
+            }
+            else{
+                j=j+2;
+                z=0;
+            }
+            i++;
         }
     }
+     /*switch(pos){
+        case 1:
+            taulell[0][0]=jugador.simbol;
+            break;
+        case 2:
+            taulell[0][2]=jugador.simbol;
+            break;
+        case 3:
+            taulell[0][4]=jugador.simbol;
+            break;
+        case 4:
+            taulell[2][0]=jugador.simbol;
+            break;
+        case 5:
+            taulell[2][2]=jugador.simbol;
+            break;
+        case 6:
+            taulell[2][4]=jugador.simbol;
+            break;
+        case 7:
+            taulell[4][0]=jugador.simbol;
+            break;
+        case 8:
+            taulell[4][2]=jugador.simbol;
+            break;
+        case 9:
+            taulell[4][4]=jugador.simbol;
+            break;
+        default:
+            break;
+    }
+
+      */
 }
